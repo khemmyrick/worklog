da39f29413c391b420ad58dfc16b93ee458ced03
Khem Myrick <khem.myrick@gmail.com>
2018-06-02 01:30:51 -0700

    Added basic functionality.
    Added search by date, date range, and REGEX functions.

--- work_log.py
+++ work_log.py
@@ -1,12 +1,15 @@
-# I've been stuck on the project forever.
-# Took longer than I'd like to admit to realize I could loop through the csv.DictReader list the same way I did in project 1.
+# As of 1:18am on 06/02/2018
 # New Entry works.
 # Search by single date works.
+# Search by date range works.
+# Search by keyword works.
+# Search by REGEX works.
+
 # Next to be implemented:
-# 1. edit/delete search entry.
-# 2. Search by date range.
-# 3. Search by keyword.
-# 4. Search by REGEX.
+# 1. Fix keyword search so that it doesn't search for patterns.
+# (Currently keyword search is virtually identical to REGEX search.)
+# 2. Add a delete entry function.
+# 3. Add an edit entry function.
 
 import datetime
 import csv
@@ -17,6 +20,50 @@ import re
 def clear_screen():
     """Clear the screen."""
     os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def edit_entry(log):
+    with open('log.csv', 'r') as logfile:
+        log_contents = list(csv.DictReader(logfile, delimiter=','))
+    for entry in log_contents:
+        if log_contents[entry] == log:
+            condemned = entry
+            new_date = input('''
+{} is the date for this entry.
+Type new date 'MM/DD/YYYY' to change, or just press enter.
+'''.format(log['Date']))
+            new_task = input('''
+{} is the task name for this entry.
+Type new task name to change, or just press enter.
+'''.format(log['Task']))
+            new_minutes = input('''
+{} is the number of minutes spent on this task.
+Type a new integer to edit, or just press enter to skip.
+'''.format(log['Time Spent']))
+            new_notes = input('''
+"{}" 
+These are the task notes for this entry.
+Type new notes to replace, or press enter to skip.
+'''.format(log['Notes']))
+    # still to do:
+    # open log.csv and delete the row number that matches 'condemned' variable
+    # Should I delete condemned row and add edited row to log_contents and copy the result back into log.csv?
+
+    with open('log.csv', 'a') as logfile:
+        fieldnames = ['Date', 'Task', 'Time Spent', 'Notes']
+        logwriter = csv.DictWriter(logfile, fieldnames=fieldnames)
+    # add edited row to the bottom of log.csv
+        logwriter.writerow({
+            'Date': new_date,
+            'Task': new_task,
+            'Time Spent': new_minutes,
+            'Notes': new_notes
+            })
+    return "Entry edited."
+
+
+def delete_entry(log):
+    pass
 
 
 def display_results(results):
@@ -32,17 +79,17 @@ def display_results(results):
         print("Time Spent: " + results[entry]['Time Spent'])
         print("Notes: " + results[entry]['Notes'] + "\n")
         print("Entry {} of {}.".format((entry + 1), len(results)))
-        if entry == 0 & entry < (len(results) - 1):
+        if entry == 0 and entry < (len(results) - 1):
             review_action = input('''
 [N]ext Item,
 [E]dit Item, [D]elete Item, [R]eturn to Search Menu
 > ''')
-        elif entry == (len(results) - 1) & entry > 0:
+        elif entry == (len(results) - 1) and entry > 0:
             review_action = input('''
 [P]revious Item,
 [E]dit Item, [D]elete Item, [R]eturn to Search Menu
 > ''')
-        elif entry == (len(results) - 1) & entry == 0:
+        elif entry == (len(results) - 1) and entry == 0:
             review_action = input('''
 [E]dit Item, [D]elete Item, [R]eturn to Search Menu
 > ''')
@@ -51,21 +98,30 @@ def display_results(results):
 [N]ext Item, [P]revious Item,
 [E]dit Item, [D]elete Item, [R]eturn to Search Menu
 > ''')
-        if review_action.upper() == "N":
+        if review_action.upper() == "N" and entry < len(results) - 1:
             entry += 1
-        elif review_action.upper() == "P":
+        elif review_action.upper() == "P" and entry > 0:
             entry -= 1
         elif review_action.upper() == "E":
-            edit_entry()
+            edit_entry(results[entry])
             pass
         elif review_action.upper() == "D":
-            delete_entry()
+            delete_entry(results[entry])
             pass
         elif review_action.upper() == "R":
             results_loop -= 1
             return "Search complete."
         else:
-            print("Error: Please select a valid option.")
+            input("Error: Invalid selection. Press enter to try again.")
+
+
+def check_for_results(results):
+    if results == []:
+        input("No matches. Press enter to return to search menu.")
+        return "No matches found."
+    else:
+        display_results(results)
+        return "Returning to search menu."
 
 
 def search_date(log_contents):
@@ -75,15 +131,15 @@ def search_date(log_contents):
     while search_loop:
         clear_screen()
         target_date = input('''
-    Please enter date in following format:
-    MM/DD/YYYY
-    > ''')
+Please enter date in following format:
+MM/DD/YYYY
+> ''')
         date_fmt = r'\d{2}/\d{2}/\d{4}'
         if re.match(date_fmt, target_date):
             for entry in log_contents:
                 if entry['Date'] == target_date:
                     results.append(entry)
-            display_results(results)
+            check_for_results(results)
             search_loop -= 1
         else:
             print("Error: Invalid date. \n Please enter valid date.")
@@ -91,24 +147,65 @@ def search_date(log_contents):
 
 
 def search_date_range(log_contents):
-    range_fmt = r'\d{2}/\d{2}/\d{4}(-\d{2}/\d{2}/\d{4})?'
-    target_range = input('''
+    """Search by date range."""
+    results = []
+    search_loop = 1
+    range_fmt = r'\d{2}/\d{2}/\d{4}-\d{2}/\d{2}/\d{4}'
+    input_format = '%m/%d/%Y-%m/%d/%Y'
+    while search_loop:
+        clear_screen()
+        target_range = input('''
 Please enter date range in following format:
 MM/DD/YYYY-MM/DD/YYYY
-''')
-    pass
+> ''')
+        # datestring = datetime.datetime.strptime(day, input_format)
+        if re.match(range_fmt, target_range):
+            targets = target_range.split(sep='-')
+            date_first = datetime.datetime.strptime(targets[0], "%m/%d/%Y")
+            date_last = datetime.datetime.strptime(targets[1], "%m/%d/%Y")
+            # use datetime string capture to get date objects
+            for entry in log_contents:
+                # convert entry['date'] into datetime object for next line.
+                dtlog = datetime.datetime.strptime(entry['Date'], "%m/%d/%Y")
+                if dtlog >= date_first and dtlog <= date_last:
+                    results.append(entry)
+            check_for_results(results)
+            search_loop -= 1
+        else:
+            print("Error: Invalid dates. \n Please enter valid date.")
+            input("Press 'enter' to retry.")
 
 
 def search_term(log_contents):
     """Search by keyword(s)."""
-    pass
-    target_term = input("Please enter search terms.")
+    results = []
+    search_loop = 1
+    while search_loop:
+        clear_screen()
+        target_term = input("Please enter search terms. > ")
+        for entry in log_contents:
+            if re.search(target_term,
+                         entry['Task']) or re.search(target_term,
+                                                     entry['Notes']):
+                results.append(entry)
+        check_for_results(results)
+        search_loop -= 1
 
 
 def search_regex(log_contents):
-    """Search by regex codes?"""
-    # ?
-    pass
+    """Search by regex strings?"""
+    results = []
+    search_loop = 1
+    while search_loop:
+        clear_screen()
+        target_term = input("Please enter a valid REGEX pattern. > ")
+        for entry in log_contents:
+            if re.search(target_term,
+                         entry['Task']) or re.search(target_term,
+                                                     entry['Notes']):
+                results.append(entry)
+        check_for_results(results)
+        search_loop -= 1
 
 
 def new_entry():
@@ -159,14 +256,14 @@ def search_entries():
         else:
             clear_screen()
             search_type = input("""
-    How would you like to search? Type a letter to select your choice.
-    ----------------------------
-    A) By Date
-    B) By Range of Dates
-    C) By Search Keyword
-    D) By REGEX Pattern
-    E) Return to Main Menu
-    > """)
+How would you like to search? Type a letter to select your choice.
+----------------------------
+A) By Date
+B) By Range of Dates
+C) By Search Keyword
+D) By REGEX Pattern
+E) Return to Main Menu
+> """)
 
         if search_type.upper() == 'A':
             search_date(log_contents)
