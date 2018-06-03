084dff170771339cbd301362e77502c17dcedef5
Khem Myrick <khem.myrick@gmail.com>
2018-06-02 23:03:12 -0700

    Added edit and delete functions.
    Edit and delete functions added.
    
    Search by keyword now ignores capitalization.
    
    main_menu() now runs inside of __name__ == '__main__' block

--- work_log.py
+++ work_log.py
@@ -1,15 +1,8 @@
-# As of 1:18am on 06/02/2018
-# New Entry works.
-# Search by single date works.
-# Search by date range works.
-# Search by keyword works.
-# Search by REGEX works.
-
-# Next to be implemented:
-# 1. Fix keyword search so that it doesn't search for patterns.
-# (Currently keyword search is virtually identical to REGEX search.)
-# 2. Add a delete entry function.
-# 3. Add an edit entry function.
+# As of 10:47pm on 06/02/2018
+# Thanks to Chris Howell on the review-my-project slack for looking over the previous version.
+# Still to do: Prevent New Entry or Edit entry from saving invalid strings in dates/minutes sections.
+# Maybe recreate the way that main menu and search menu currently catch invalid options?
+# Do keyword or REGEX search sections need anything else?
 
 import datetime
 import csv
@@ -23,47 +16,100 @@ def clear_screen():
 
 
 def edit_entry(log):
+    """Edit log entry."""
     with open('log.csv', 'r') as logfile:
         log_contents = list(csv.DictReader(logfile, delimiter=','))
+    row_iter = 0
+
     for entry in log_contents:
-        if log_contents[entry] == log:
-            condemned = entry
-            new_date = input('''
+        if entry == log:
+            condemned = row_iter
+
+            nd_loop = 1
+            date_fmt = r'(\d{2}/\d{2}/\d{4})?'
+            while nd_loop:
+                clear_screen()
+                new_date = input('''
 {} is the date for this entry.
 Type new date 'MM/DD/YYYY' to change, or just press enter.
 '''.format(log['Date']))
+                if re.match(date_fmt, new_date):
+                    nd_loop -= 1
+                else:
+                    input('Invalid date. Press enter to try again.')
+
             new_task = input('''
 {} is the task name for this entry.
 Type new task name to change, or just press enter.
 '''.format(log['Task']))
-            new_minutes = input('''
+
+            nm_loop = 1
+            nm_fmt = r'\d*'
+            while nm_loop:
+                new_minutes = input('''
 {} is the number of minutes spent on this task.
 Type a new integer to edit, or just press enter to skip.
 '''.format(log['Time Spent']))
+                if re.match(nm_fmt, new_minutes):
+                    nm_loop -= 1
+                else:
+                    input('Invalid integer. Press enter to try again.')
+
             new_notes = input('''
-"{}" 
+"{}"
 These are the task notes for this entry.
 Type new notes to replace, or press enter to skip.
 '''.format(log['Notes']))
-    # still to do:
-    # open log.csv and delete the row number that matches 'condemned' variable
-    # Should I delete condemned row and add edited row to log_contents and copy the result back into log.csv?
+            new_row = {'Date': new_date,
+                       'Task': new_task,
+                       'Time Spent': new_minutes,
+                       'Notes': new_notes}
+        else:
+            row_iter += 1
 
-    with open('log.csv', 'a') as logfile:
+    if new_row['Date'] == '':
+        new_row['Date'] = log_contents[condemned]['Date']
+    if new_row['Task'] == '':
+        new_row['Task'] = log_contents[condemned]['Task']
+    if new_row['Time Spent'] == '':
+        new_row['Time Spent'] = log_contents[condemned]['Time Spent']
+    if new_row['Notes'] == '':
+        new_row['Notes'] = log_contents[condemned]['Notes']
+
+    log_contents.pop(condemned)
+    log_contents.append(new_row)
+
+    with open('log.csv', 'w') as logfile:
         fieldnames = ['Date', 'Task', 'Time Spent', 'Notes']
         logwriter = csv.DictWriter(logfile, fieldnames=fieldnames)
-    # add edited row to the bottom of log.csv
-        logwriter.writerow({
-            'Date': new_date,
-            'Task': new_task,
-            'Time Spent': new_minutes,
-            'Notes': new_notes
-            })
-    return "Entry edited."
+        logwriter.writeheader()
+        for row in log_contents:
+            logwriter.writerow(row)
+    input('Entry edited. Press enter to return to search menu.')
+    return
 
 
 def delete_entry(log):
-    pass
+    """Delete an entry."""
+    with open('log.csv', 'r') as logfile:
+        log_contents = list(csv.DictReader(logfile, delimiter=','))
+    row_iter = 0
+    for entry in log_contents:
+        if entry == log:
+            condemned = row_iter
+        else:
+            row_iter += 1
+
+    log_contents.pop(condemned)
+
+    with open('log.csv', 'w') as logfile:
+        fieldnames = ['Date', 'Task', 'Time Spent', 'Notes']
+        logwriter = csv.DictWriter(logfile, fieldnames=fieldnames)
+        logwriter.writeheader()
+        for row in log_contents:
+            logwriter.writerow(row)
+    input('Entry deleted.  Press enter to return to search menu.')
+    return
 
 
 def display_results(results):
@@ -79,6 +125,7 @@ def display_results(results):
         print("Time Spent: " + results[entry]['Time Spent'])
         print("Notes: " + results[entry]['Notes'] + "\n")
         print("Entry {} of {}.".format((entry + 1), len(results)))
+
         if entry == 0 and entry < (len(results) - 1):
             review_action = input('''
 [N]ext Item,
@@ -98,36 +145,38 @@ def display_results(results):
 [N]ext Item, [P]revious Item,
 [E]dit Item, [D]elete Item, [R]eturn to Search Menu
 > ''')
+
         if review_action.upper() == "N" and entry < len(results) - 1:
             entry += 1
         elif review_action.upper() == "P" and entry > 0:
             entry -= 1
         elif review_action.upper() == "E":
             edit_entry(results[entry])
-            pass
+            results_loop -= 1
         elif review_action.upper() == "D":
             delete_entry(results[entry])
-            pass
+            results_loop -= 1
         elif review_action.upper() == "R":
             results_loop -= 1
-            return "Search complete."
         else:
             input("Error: Invalid selection. Press enter to try again.")
 
 
 def check_for_results(results):
+    """Check if any search results are caught."""
     if results == []:
         input("No matches. Press enter to return to search menu.")
-        return "No matches found."
+        return
     else:
         display_results(results)
-        return "Returning to search menu."
+        return
 
 
 def search_date(log_contents):
     """Search by date."""
     results = []
     search_loop = 1
+
     while search_loop:
         clear_screen()
         target_date = input('''
@@ -135,12 +184,14 @@ Please enter date in following format:
 MM/DD/YYYY
 > ''')
         date_fmt = r'\d{2}/\d{2}/\d{4}'
+
         if re.match(date_fmt, target_date):
             for entry in log_contents:
                 if entry['Date'] == target_date:
                     results.append(entry)
             check_for_results(results)
             search_loop -= 1
+
         else:
             print("Error: Invalid date. \n Please enter valid date.")
             input("Press 'enter' to retry.")
@@ -151,26 +202,25 @@ def search_date_range(log_contents):
     results = []
     search_loop = 1
     range_fmt = r'\d{2}/\d{2}/\d{4}-\d{2}/\d{2}/\d{4}'
-    input_format = '%m/%d/%Y-%m/%d/%Y'
+
     while search_loop:
         clear_screen()
         target_range = input('''
 Please enter date range in following format:
 MM/DD/YYYY-MM/DD/YYYY
 > ''')
-        # datestring = datetime.datetime.strptime(day, input_format)
+
         if re.match(range_fmt, target_range):
             targets = target_range.split(sep='-')
             date_first = datetime.datetime.strptime(targets[0], "%m/%d/%Y")
             date_last = datetime.datetime.strptime(targets[1], "%m/%d/%Y")
-            # use datetime string capture to get date objects
             for entry in log_contents:
-                # convert entry['date'] into datetime object for next line.
                 dtlog = datetime.datetime.strptime(entry['Date'], "%m/%d/%Y")
                 if dtlog >= date_first and dtlog <= date_last:
                     results.append(entry)
             check_for_results(results)
             search_loop -= 1
+
         else:
             print("Error: Invalid dates. \n Please enter valid date.")
             input("Press 'enter' to retry.")
@@ -180,13 +230,15 @@ def search_term(log_contents):
     """Search by keyword(s)."""
     results = []
     search_loop = 1
+
     while search_loop:
         clear_screen()
         target_term = input("Please enter search terms. > ")
         for entry in log_contents:
-            if re.search(target_term,
-                         entry['Task']) or re.search(target_term,
-                                                     entry['Notes']):
+            if re.search(
+                target_term.lower(),
+                entry['Task'].lower()) or re.search(target_term.lower(),
+                                                    entry['Notes'].lower()):
                 results.append(entry)
         check_for_results(results)
         search_loop -= 1
@@ -196,6 +248,7 @@ def search_regex(log_contents):
     """Search by regex strings?"""
     results = []
     search_loop = 1
+
     while search_loop:
         clear_screen()
         target_term = input("Please enter a valid REGEX pattern. > ")
@@ -210,13 +263,12 @@ def search_regex(log_contents):
 
 def new_entry():
     """Add new entry to work log."""
-    # Format their entry into a date object?
     try:
         with open('log.csv', 'r') as logfile:
             demostring = logfile.read()
     except FileNotFoundError:
         demostring = ''
-        # Check if log.csv exists.
+
     with open('log.csv', 'a') as logfile:
         fieldnames = ['Date', 'Task', 'Time Spent', 'Notes']
         logwriter = csv.DictWriter(logfile, fieldnames=fieldnames)
@@ -224,9 +276,9 @@ def new_entry():
         task_title = input("Please enter a title for this task. > ")
         task_time = input("Please enter number of minutes spent on task. > ")
         task_notes = input("Any additional notes? (Press enter to skip). > ")
+
         if demostring == '':
             logwriter.writeheader()
-            # If log.csv doesn't exist, create it and add header row.
         logwriter.writerow({
             'Date': task_date,
             'Task': task_title,
@@ -234,8 +286,8 @@ def new_entry():
             'Notes': task_notes
             })
 
-    input("Entry saved.  Press enter to return to menu.")
-    return task_title + " saved."
+    input("{} saved.  Press enter to return to menu.".format(task_title))
+    return
 
 
 def search_entries():
@@ -245,8 +297,6 @@ def search_entries():
 
         try:
             with open('log.csv', 'r') as logfile:
-                # Reopen csv file and check contents every time.
-                # Reason being: We may have made edits!
                 log_contents = list(csv.DictReader(logfile, delimiter=','))
 
         except FileNotFoundError:
@@ -275,14 +325,17 @@ E) Return to Main Menu
             search_regex(log_contents)
         elif search_type.upper() == 'E':
             clear_screen()
-            return "Return to Main Menu. \n ------------------------"
+            return
         else:
-            print("Invalid entry.  Please select either option A, B, C, or D.")
+            print(
+                "Invalid entry.  Please select either option A, B, C, D, or E"
+            )
 
 
 def main_menu():
     """Start the app."""
     while True:
+        clear_screen()
         start = input("""
 WORK LOG
 Please select from the following options:
@@ -295,6 +348,8 @@ C) Exit Program
 ----------------------------
 > """)
         if start.upper() == "C":
+            clear_screen()
+            print('Farewell!')
             quit()
         elif start.upper() == "A":
             print(new_entry())
@@ -302,9 +357,7 @@ C) Exit Program
             print(search_entries())
         else:
             print("Invalid entry.  Please select either option A, B, or C.")
-            print('*' * 50)
+            input('Press enter to start again.')
 
-
-main_menu()
-
-# Follow PEP8
+if __name__ == '__main__':
+    main_menu()
